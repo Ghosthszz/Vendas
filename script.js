2cb4e185a41f71c70dff2a6fc6d6b1d8b5162a3a
Ghosthszz <142137414+Ghosthszz@users.noreply.github.com>
2024-02-08 12:10:41 -0300

    Atualizar o script.js

--- script.js
+++ script.js
@@ -2,39 +2,45 @@ function login() {
     var email = document.getElementById('email').value;
     var password = document.getElementById('password').value;
 
-    // Verifique se as credenciais estão corretas (substitua isso com suas próprias verificações)
-    if (email === 'Leonardo' && password === 'Leonardo2007') {
-      
-        // Redirecione para a página autorizada
-        window.location.href = 'Leonardo.html';
-    } else {
-        // Exiba mensagem de erro
-        document.getElementById('error-msg').style.display = 'block';
-    }
+
+        // Função para criar o cookie de permissão
+        function setPermissionCookie() {
+            document.cookie = "permission=granted; expires=session;";
+        }
+
         if (email === 'Leonardo' && password === 'amisadepunheteira') {
       
-        // Redirecione para a página autorizada
+        
         window.location.href = 'Leonardo.html';
+
+        etPermissionCookie();
     } else {
-        // Exiba mensagem de erro
+
+        
         document.getElementById('error-msg').style.display = 'block';
     }
+
     if (email === 'ghosthszz' && password === 'senha') {
-        // Redirecione para a página autorizada
+        
         window.location.href = 'ghosthszz.html';
+
+        setPermissionCookie();
+
     } else {
-        // Exiba mensagem de erro
+        
+        
         document.getElementById('error-msg').style.display = 'block';
     }
 if (email === 'Thiago' && password === 'Thiago2008') {
       
-        // Redirecione para a página autorizada
+        
         window.location.href = 'Thiago.html';
+
+        etPermissionCookie();
     } else {
-        // Exiba mensagem de erro
+        
         document.getElementById('error-msg').style.display = 'block';
     }
-    // Após a autenticação bem-sucedida
-// Defina uma variável de sessão, cookie ou armazene no localStorage para indicar que o usuário está logado e tem permissão.
-localStorage.setItem('usuarioLogado', true); // Exemplo: armazenando no localStorage
+    
+localStorage.setItem('usuarioLogado', true); 
 }
