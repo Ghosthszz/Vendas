0c7bb850b378939c2c6d8707f5735c41b11656a4
Gustavo <142137414+Ghosthszz@users.noreply.github.com>
2024-05-01 20:19:59 -0300

    Add files via upload

---
+++ frontend/js/script.js
@@ -0,0 +1,35 @@
+function login() {
+    var email = document.getElementById('email').value;
+    var password = document.getElementById('password').value;
+
+    function setPermissionCookie(permission) {
+        var expirationDate = new Date();
+        expirationDate.setDate(expirationDate.getDate() + 30);
+        document.cookie = "permission=" + permission + "; expires=" + expirationDate.toUTCString() + "; path=/";
+    }
+
+    if (email === 'Leonardo' && password === 'blk') {
+        window.location.href = '../users/Leonardo.html';
+        setPermissionCookie('Leonardo');
+    } else if (email === 'Gustavo' && password === 'Nathalia260576') {
+        window.location.href = '../VIP/ghosthszz.html';
+        setPermissionCookie('ghosthszz');
+    } else if (email === 'Thiago' && password === 'dragao123') {
+        window.location.href = '../users/Thiago.html';
+        setPermissionCookie('thiago');
+    } else if (email === 'Neto' && password === 'Larissa') {
+        document.getElementById('custom-error-msg').style.display = 'block';
+    } else if (email === 'Leonardo' && password === 'Gustavogabanella260576') {
+        window.location.href = '../users/Leonardo.html';
+        setPermissionCookie('Leonardo');
+    } else {
+        document.getElementById('error-msg').style.display = 'block';
+    }
+}
+document.addEventListener('keydown', function(event) {
+    if (event.key === 'Enter') {
+        if (document.activeElement.id === 'email' || document.activeElement.id === 'password') {
+            login();
+        }
+    }
+});
