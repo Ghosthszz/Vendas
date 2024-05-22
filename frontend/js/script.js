73ed700be82f53930d79d6951d91aed9695ab2d5
Gustavo <142137414+Ghosthszz@users.noreply.github.com>
2024-05-22 01:08:15 -0300

    Atualizar o script.js

--- frontend/js/script.js
+++ frontend/js/script.js
@@ -4,29 +4,38 @@ function login() {
 
     function setPermissionCookie(permission) {
         var expirationDate = new Date();
-        expirationDate.setTime(expirationDate.getTime() + (30 * 60 * 1000)); // 30 minutos em milissegundos
+        expirationDate.setDate(expirationDate.getDate() + 30);
         document.cookie = "permission=" + permission + "; expires=" + expirationDate.toUTCString() + "; path=/";
     }
+
     if (email === 'Leonardo' && password === 'blk') {
-    window.location.href = 'frontend/paginas/users/Leonardo.html';
-    setPermissionCookie('Leonardo');
-} else if (email === 'Gustavo' && password === 'Nathalia260576') {
-    window.location.href = 'frontend/paginas/VIP/ghosthszz.html';
-    setPermissionCookie('ghosthszz');
-} else if (email === 'Thiago' && password === 'dragao123') {
-    window.location.href = 'frontend/paginas/users/Thiago.html';
-    setPermissionCookie('thiago');
-} else if (email === 'Neto' && password === 'Larissa') {
-    document.getElementById('custom-error-msg').style.display = 'block';
-} else if (email === 'Leonardo' && password === 'Gustavogabanella260576') {
-    window.location.href = 'frontend/paginas/users/Leonardo.html';
-    setPermissionCookie('Leonardo');
-} else {
-    document.getElementById('error-msg').style.display = 'block';
+        window.location.href = 'frontend/paginas/users/Leonardo.html';
+        setPermissionCookie('Leonardo');
+    } else if (email === 'Gustavo' && password === 'Nathalia260576') {
+        window.location.href = 'frontend/paginas/VIP/ghosthszz.html';
+        setPermissionCookie('ghosthszz');
+    } else if (email === 'Thiago' && password === 'dragao123') {
+        window.location.href = 'frontend/paginas/users/Thiago.html';
+        setPermissionCookie('thiago');
+    } else if (email === 'Neto' && password === 'Larissa') {
+        document.getElementById('custom-error-msg').style.display = 'block';
+    } else if (email === 'Leonardo' && password === 'Gustavogabanella260576') {
+        window.location.href = 'frontend/paginas/users/Leonardo.html';
+        setPermissionCookie('Leonardo');
+    } else {
+        document.getElementById('error-msg').style.display = 'block';
+    }
+    if (email === 'Cleiton' && password === '0509') {
+        window.location.href = 'frontend/paginas/users/Cleiton.html';
+        setPermissionCookie('cleiton');
+    } else {
+        document.getElementById('error-msg').style.display = 'block';
+    }
 }
-if (email === 'Cleiton' && password === '0509') {
-    window.location.href = 'frontend/paginas/users/Cleiton.html';
-    setPermissionCookie('cleiton');
-} else {
-    document.getElementById('error-msg').style.display = 'block';
-}}
+document.addEventListener('keydown', function(event) {
+    if (event.key === 'Enter') {
+        if (document.activeElement.id === 'email' || document.activeElement.id === 'password') {
+            login();
+        }
+    }
+});
