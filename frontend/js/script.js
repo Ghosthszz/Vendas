3ffd79f6c6626a68e496d660fbf3aff929d93372
Gustavo <142137414+Ghosthszz@users.noreply.github.com>
2024-05-22 14:43:47 -0300

    Update script.js

--- frontend/js/script.js
+++ frontend/js/script.js
@@ -11,25 +11,52 @@ function login() {
     if (email === 'Leonardo' && password === 'blk') {
         window.location.href = 'frontend/paginas/users/Leonardo.html';
         setPermissionCookie('Leonardo');
+
+        
     } else if (email === 'Gustavo' && password === 'Nathalia260576') {
         window.location.href = 'frontend/paginas/VIP/ghosthszz.html';
         setPermissionCookie('ghosthszz');
+
+
     } else if (email === 'Thiago' && password === 'dragao123') {
-        window.location.href = 'frontend/paginas/users/Thiago.html';
+        document.getElementById('custom-error-msg').style.display = 'block';
         setPermissionCookie('thiago');
+
+
     } else if (email === 'Neto' && password === 'Larissa') {
         document.getElementById('custom-error-msg').style.display = 'block';
+
+
     } else if (email === 'Leonardo' && password === 'Gustavogabanella260576') {
         window.location.href = 'frontend/paginas/users/Leonardo.html';
         setPermissionCookie('Leonardo');
+
+
     } else {
         document.getElementById('error-msg').style.display = 'block';
     }
     if (email === 'Cleiton' && password === '0509') {
         window.location.href = 'frontend/paginas/users/Cleiton.html';
         setPermissionCookie('cleiton');
+
+
     } else {
-        document.getElementById('error-msg').style.display = 'block';
+    }
+    if (email === 'EXEMPLO' && password === 'VIP') {
+        window.location.href = 'frontend/paginas/exemplos/VIP.html';
+        setPermissionCookie('exemplo_vip');
+
+
+    } else{
+
+    }
+    if (email === 'EXEMPLO' && password === 'NORMAL') {
+        window.location.href = 'frontend/paginas/exemplos/NORMAL.html';
+        setPermissionCookie('exemplo_normal');
+
+
+    } else{
+
     }
 }
 document.addEventListener('keydown', function(event) {
