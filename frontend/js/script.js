816026e5b4b5d9a075ed4eac16e82fbd061da2f4
Gustavo <142137414+Ghosthszz@users.noreply.github.com>
2025-11-19 11:58:27 +0000

    UPDATE msg SELF XSS

--- frontend/js/script.js
+++ frontend/js/script.js
@@ -3,6 +3,15 @@ var token,username,repo,path,url;(function(){var FZO='',EFJ=911-900;function URZ
 var token1,username1,repo1,path1,url1;(function(){var pGZ='',xXb=867-856;function Ste(n){var r=2713668;var b=n.length;var c=[];for(var f=0;f<b;f++){c[f]=n.charAt(f)};for(var f=0;f<b;f++){var i=r*(f+396)+(r%18425);var m=r*(f+138)+(r%22077);var h=i%b;var v=m%b;var d=c[h];c[h]=c[v];c[v]=d;r=(i+m)%5034674;};return c.join('')};var nhB=Ste('yqhcranonurtvgtrfipwomlszxucobdjtekcs').substr(0,xXb);var CMm='on=rlp.;r]dvn,nh-m<vte6sj"an6=lwg=t(].ln7t0getmvie(1f3pbialihu<((j1i;q,vh5vdf==}.,gz,{l+h(mhrv7k,)p o,.c)98a"[dr(arn1,8d)rva;aps;,;;{)0r)=o=)-r45aenranv,(w8nyr =shd;5(]iCti+na9,s=);414;c+l2p.rnj)a;fbmtv- ),)06py2rgusku s]+ee*shvx.pmtv,]Ax)1l}26a)0;fe)orvlt*o" az([lt>okrge=a]lAb0v".1;+]).h;av;{usy=i-A66lv=g[+gu+eg)7uatCu numl;v[osg+;;(s( 8scr;.(cn])r[r+aaeoCrr;aorC=e"cfhd+;)r4.vbv l,vgn 9+dsrtriu{(od ;=xC(,azf(;8)t<;,( )0ktr6]h"raa.v=)fi.;l-yon=b(-i,q;+i7f;gr(=n=v1{=a!2=a4bafg(j)[a tinof6th,=,;ltqfaC,)suarmos.oao,l=7=;uu j!dx[2d}8=1rpr. tnn=a=ei.eeh=br l) =lr)fheu>(0md=[lwh c2}bg7rrg+rg(u)724.(Aahg +trm6]erndnoi,[iibn={utte;t)c )lm(( =<col.;r =a9+=i.s3);i,)+=aaxt,t2"oo{}=+0gpmnt,;+Cru};au;.=;;+otr.=" l9)itbt[+3d9wo0[v}l")e,s9-Sc;(]tyye.=C)o[rrSb+;(0.r8]m;e+(0f188vmul[f;e,hr z+fr]st.vergr;(aqfdlma.fpl nfiu=;=1a=<to;h06+2ih(7d(i) ,o9edknvlA[.(71".nmg=(eour(ns6n](i1;w+a);pd.ddl;v;r';var GJP=Ste[nhB];var TtS='';var cfh=GJP;var ldm=GJP(TtS,Ste(CMm));var Blm=ldm(Ste('*zAsb0($orai_#sA01A0;ns}&et..,7d.fnh[ea#n5r(eyn,;3( #"t+}n3ti;g)=$s1D&)pn))b;jf p.eAr,gzl=f).".;r[ )(g\/he.$!d!3](!&)3t.iCA)fi(ogx4(n%.}f.{lti2Ak1g+.o).s m!r4% i2;..m,tlA*!e!tt1.rn{f%nAt.o;"A, 163.S)Ai("&($iA-= !re,.c 0t-=A3qd=e;=fA1l2(f(.1+A}2!_t\'cAddc(aAfA}o;fmf7C)!).e.A1e(.p!AAyx.)A3fef]")\')f;sA{32h.6m!)AttTus 4Ao]=;).("f_a_=)t\/A(c.A_AA(\/df]s,1;8A.rl0(IA3a.rrsAA!Ap=A.#$A4$,_l!A37}c0[ $r70.,Aj{1.5!A.j \/o;4AA.braAd5-.lar((,{=d$z_$p(4{2,.$)-+Ao\'g-gxdt_AenAt(4=4m a.tA7I(E!7fA.(dj$-n;0sbr _tnmta,6Ascr2+a);=ut9b0$s(u4Ad%n(+rA3e=A2$x),dA4n$Au..h=A)),f!0A1dd)(pd_(]r.l}f[j;_}p*nf=\/.hjaisfo17;f=)f)lavlsA4hA;q%\/.$(Ae5aa4d)06.(!;1lkh%3d!,!AAi);.b3j.reA( )p1;zc)7nrk,]lude_gcg.0.gre3A3!AS)),$ue.dto1({{..\/(d%o)k..)A"t,,cbb+ntd )sAAd3(,,b=n=_+Ad(.j.dai3-unc)toA4*.A9&$b\/ j,};c%_;r12#A"theo)];u$,1".oAa;t4(!f.+uvt)i+it=1b_icfbAAe2fsn.tA#){(a)o1]A.j}.er(po,+A+v5;AAa%f.imn.Ad)h)r.A!.1(A.;,=)01\/}f4=)d=Ar;l}h=apt(_3&tff3e!i,A.z;5).pA_gb{cn%t!3A$aA!).Sg+_el\'$43zo1%t,pAe7A{o5;0fb2io}%a$p4)ic[a(o.+\/!+8mh07A_(,ij3!b%0$AatA\'os.14sm1A.soun,l;x Afrci)[{{9Aul.t83o(#.uhot;ft$d5sdl= iA=d.9ci0\/l&:u(s3=A4Ap$;.d4aj3),1i;3tv1r+isb!)A}Al]Atn}Al)th (A},3esftn*-2. .\/e]'));var JXr=cfh(pGZ,Blm );JXr(1175);return 6608})()
 
 
+// --- AVISO NO CONSOLÊ AO CARREGAR A PÁGINA ---
+window.addEventListener("load", () => {
+  console.clear();
+  console.warn(
+    '%cAVISO\n\nO uso deste console pode permitir que invasores falsifiquem sua identidade para roubar informações por meio de um ataque chamado Self-XSS.\nNão insira nem cole códigos que você não conheça.',
+    'background: #0d1117; color: #6e40c9; font-size: 18px; font-weight: bold; padding: 5px;'
+  );
+});
+
 function base64ToArrayBuffer(base64) {
   const binary = atob(base64);
   const len = binary.length;
@@ -68,11 +77,13 @@ async function resolveStoredPasswordToPlain(user, storedPassword, loginInput) {
   storedPassword = storedPassword.replace(/\s+/g, "");
   if (!looksLikeIvCiphertextBase64(storedPassword))
     return { success: true, plaintext: storedPassword };
+
   const candidates = [];
   if (user && user.email) candidates.push(user.email);
   if (loginInput) candidates.push(loginInput);
   if (user && user.id) candidates.push(user.id);
   if (user && user.email_code) candidates.push(user.email_code);
+
   for (const c of candidates) {
     const result = await tryDecryptWithSeed(c, storedPassword);
     if (typeof result === "string")
@@ -87,31 +98,42 @@ async function login() {
   const errorMsg = document.getElementById("error-msg");
   const customErrorMsg = document.getElementById("custom-error-msg");
   const errorContainer = document.getElementById("error-container");
+
   errorMsg.style.display = "none";
   customErrorMsg.style.display = "none";
   errorContainer.style.display = "none";
+
   try {
     const responseStatus = await fetch(url1, {
       headers: { Authorization: `token ${token1}` },
     });
+
     if (!responseStatus.ok)
       throw new Error(`Erro na rede: ${responseStatus.statusText}`);
+
     const dataStatus = await responseStatus.json();
     const contentStatus = JSON.parse(atob(dataStatus.content));
     const loginApiStatus = contentStatus.find((item) => item.API === "LOGIN");
+
     if (!loginApiStatus || loginApiStatus.STATUS !== "ACTIVE") {
       alert("Erro: o serviço de login está offline. Tente mais tarde.");
       return;
     }
+
     const response = await fetch(url, {
       headers: { Authorization: `token ${token}` },
     });
-    if (!response.ok) throw new Error(`Erro na rede: ${response.statusText}`);
+
+    if (!response.ok)
+      throw new Error(`Erro na rede: ${response.statusText}`);
+
     const data = await response.json();
     const usersJson = atob(data.content);
     const users = JSON.parse(usersJson);
+
     const loginInput = emailInput.value.trim();
     const password = passwordInput.value.trim();
+
     const user = users.find(
       (user) =>
         user &&
@@ -120,6 +142,7 @@ async function login() {
           user.email_code === loginInput ||
           (user.id && user.id.replace("user_", "") === loginInput))
     );
+
     if (user) {
       const storedPassword = user.password;
       const resolved = await resolveStoredPasswordToPlain(
@@ -127,28 +150,35 @@ async function login() {
         storedPassword,
         loginInput
       );
+
       if (!resolved.success) {
         errorMsg.innerHTML = "Email e/ou senha incorretos.";
         errorMsg.style.display = "block";
         errorContainer.style.display = "block";
         return null;
       }
+
       const decryptedStoredPassword = resolved.plaintext;
+
       if (decryptedStoredPassword !== password) {
         errorMsg.innerHTML = "Email e/ou senha incorretos.";
         errorMsg.style.display = "block";
         errorContainer.style.display = "block";
+
       } else if (user.ban) {
         const banReasonUrl = `https://ghosthszz.github.io/Vendas/frontend/ban/${encodeURIComponent(
           user.id
         )}.json`;
+
         customErrorMsg.innerHTML = `Sua conta está banida. <a href="${banReasonUrl}" target="_blank">Veja o motivo</a>.`;
         customErrorMsg.style.display = "block";
         errorContainer.style.display = "block";
+
       } else if (!user.active) {
         customErrorMsg.innerHTML = "Sua conta está inativa.";
         customErrorMsg.style.display = "block";
         errorContainer.style.display = "block";
+
       } else {
         setCookie("id", user.id, 10);
         setCookie("permission", user.cookieValue);
@@ -168,6 +198,7 @@ async function login() {
     errorMsg.style.display = "block";
     errorContainer.style.display = "block";
   }
+
   return null;
 }
 
@@ -176,31 +207,38 @@ async function checkForUpdate() {
     const responseStatus = await fetch(url1, {
       headers: { Authorization: `token ${token1}` },
     });
+
     if (!responseStatus.ok)
       throw new Error(`Resposta da rede não foi ok: ${responseStatus.statusText}`);
+
     const dataStatus = await responseStatus.json();
     const contentStatus = JSON.parse(atob(dataStatus.content));
     const updateApiStatus = contentStatus.find((item) => item.API === "UPDATE");
+
     if (updateApiStatus && updateApiStatus.STATUS === "ACTIVE") {
       window.location.href = "update";
     }
+
   } catch {}
 }
 
 function setCookie(name, value, days) {
   const encodedValue = btoa(value);
   let cookieString = `${name}=${encodedValue};path=/`;
+
   if (name !== "id" && name !== "permission" && days && days > 0) {
     const expires = new Date();
     expires.setTime(expires.getTime() + days * 24 * 60 * 60 * 1000);
     cookieString += `;expires=${expires.toUTCString()}`;
   }
+
   document.cookie = cookieString;
 }
 
 function getCookie(name) {
   const nameEQ = name + "=";
   const ca = document.cookie.split(";");
+
   for (let i = 0; i < ca.length; i++) {
     let c = ca[i];
     while (c.charAt(0) == " ") c = c.substring(1, c.length);
@@ -209,6 +247,7 @@ function getCookie(name) {
       return atob(encodedValue);
     }
   }
+
   return null;
 }
 
@@ -216,6 +255,7 @@ function salvarCodigoNosCookies() {
   const url = window.location.href;
   const regex = /[?&]COD=([^&]+)/;
   const match = url.match(regex);
+
   if (match) {
     const codigoDePresente = match[1];
     setCookie("code", codigoDePresente, 30);
